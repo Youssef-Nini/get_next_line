ff5a083696b7828c1612e59df93df433e9edefd2
agadir youssef <youssefagadir2005@gmail.com>
2024-12-27 09:31:52 +0000

    make new try

--- get_next_line.c
+++ get_next_line.c
@@ -3,47 +3,30 @@
 
 char *get_next_line(int fd)
 {
-	static char buffer[BUFFER_SIZE + 1];
-	char tmp[BUFFER_SIZE + 1];
-	char stock[BUFFER_SIZE + 1];
+	static char *stock = NULL;
+	char buffer[BUFFER_SIZE + 1];
+	char *tmp;
 	ssize_t bytes_read;
 	int i = 0;
 	int j = 0;
-	if (buffer[0] == '\0')
+
+	while(bytes_read = read(fd, buffer, BUFFER_SIZE) > 0)
 	{
-		bytes_read = read(fd, buffer,BUFFER_SIZE);
 		buffer[bytes_read] = '\0';
+
+		tmp = ft_strjoin(tmp, stock);
+
+		if (ft_strchr(tmp , '\n'))
+			break;
 	}
-	// printf("read :%d\n",bytes_read);
-	// printf("buffer: %s>>>>>\n ", buffer);
-	while(buffer[i] && buffer[i] != '\n')
+	while(tmp[i] && tmp[i] != '\n')
 	{
-		stock[i] = buffer[i];
+		stock[i] = tmp[i];
 		i++;
 	}
-	if (buffer[i] = '\n')
-		stock[i] = '\n';
-	else
-		stock[i] = '\0';
-	while (buffer[j] !='\0')
-	{
-		tmp[j] = buffer[j];
-		j++;
-	}
-	tmp[j] = '\0';
-	j = 0;
-	i++;
-	while(tmp[i])
-	{
-		buffer[j] = tmp[i];
-		i++;
-		j++;
-	}
-	buffer[j] = '\0';
-	// printf("buffer2: %s\n" , buffer);
-	printf("stock: %s", stock);
-	printf("buffer: %s", buffer);
-	return buffer;
+	stock[i] = '\0';
+	printf("stock : %s\n", stock);
+	return stock;
 }
 int main(){
 	int fd = open("file.txt",O_RDONLY);
